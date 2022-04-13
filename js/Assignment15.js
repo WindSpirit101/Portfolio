4d1d05ded2bd79fbc3db1066202bbb5778b0c785
WindSpirit101 <100785677+WindSpirit101@users.noreply.github.com>
2022-04-13 11:53:25 -0500

    i give up

--- js/Assignment15.js
+++ js/Assignment15.js
@@ -6,10 +6,8 @@ function showSlide(){
   let slide2 = document.getElementById('slide2');
   let slide3 = document.getElementById('slide3');
   let slide4 = document.getElementById('slide4');
-  let slide5 = document.getElementById('slide5');
-  let slide6 = document.getElementById('slide6');
 
-  let slides = [slide1, slide2, slide3, slide4, slide5, slide6];
+  let slides = [slide1, slide2, slide3, slide4];
 
   if (currentSlideIndex >= slides.length){
     currentSlideIndex = 0;
